e76dd4f3a63381919eb8eb3723fad3b1fa8896ea
Matt Fowler <mbfowler@gmail.com>
2023-09-20 18:02:11 -0400

    Add initial list styling, mock data, nanoid

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react'
+import { nanoid } from 'nanoid'
+import { Article } from '../Article/Article'
+import { mockArticleData } from './mockArticleData'
 import './Main.scss'
 
 export const Main = () => {
+    const [articlesData, setArticlesData] = useState()
+
+    useEffect(() => {
+        setArticlesData(mockArticleData.articles)
+        // fetch real data when ready
+    }, [])
+
+    const articles = articlesData?.map(article => <Article article={article} key={nanoid()} />)
+    
     return (
-        <main></main>
+        <main>
+            {articles}
+        </main>
     )
 }
