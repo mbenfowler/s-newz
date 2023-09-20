122b8ac43fc54108a415563167691c93b125352c
Matt Fowler <mbfowler@gmail.com>
2023-09-20 12:38:51 -0400

    Add initial component structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,20 @@
-import logo from '../../logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { Nav } from '../Nav/Nav'
+import { Main } from '../Main/Main'
+import { Article } from '../Article/Article'
+import { Error } from '../Error/Error'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/article/:id' element={<Article />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Error/Error.js
@@ -0,0 +1,5 @@
+export const Error = () => {
+    return (
+        <p>Something went wrong...</p>
+    )
+}
