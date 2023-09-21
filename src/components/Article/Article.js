d44d84572b6e034f657ca2743cf3adfa24765bf8
Matt Fowler <mbfowler@gmail.com>
2023-09-21 14:22:14 -0400

    Connect api to frontend

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,13 +1,17 @@
 import './Article.scss'
 
 export const Article = ({ selectedArticle }) => {
+    const handleImgError = (e) => {
+        e.target.src = `${process.env.PUBLIC_URL}/images/no-image.png`
+    }
+
     if (selectedArticle) {
         const image = selectedArticle.urlToImage ? selectedArticle.urlToImage : `${process.env.PUBLIC_URL}/images/no-image.png`
 
         if (selectedArticle.title !== '[Removed]') {
             return (
                 <article className='article'>
-                    <img src={image}></img>
+                    <img src={image} onError={handleImgError}></img>
                     <h2>{selectedArticle.title}</h2>
                     <p className='article-date-source'>{selectedArticle.publishedAt} - {selectedArticle.source.name}</p>
                     <p className='article-content'>{selectedArticle.content}</p>
